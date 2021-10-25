98cd1747648d1a56f23d2465a77db8f218936e83
paolo <ventura.paolo@gmail.com>
2021-10-25 17:02:44 +0100

    Github action setup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Task from "./components/Task";
+import { Provider } from "react-redux";
+import store from "./lib/redux";
 
-const task = {
-  id: "1",
-  title: "Paolo's Task",
-  state: "TASK_INBOX",
-  updatedAt: new Date(2021, 0, 1, 9, 0),
-};
+import InboxScreen from "./components/InboxScreen";
+
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <Task task={task} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   );
 }
-
 export default App;
