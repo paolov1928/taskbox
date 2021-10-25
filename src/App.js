b70f356008990ef5e7d1f4d97685ebddce8eb52d
paolo <ventura.paolo@gmail.com>
2021-10-25 16:00:55 +0100

    Implement the TaskList components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Task from "./components/Task";
+
+const task = {
+  id: "1",
+  title: "Paolo's Task",
+  state: "TASK_INBOX",
+  updatedAt: new Date(2021, 0, 1, 9, 0),
+};
 
 function App() {
   return (
     <div className="App">
+      <Task task={task} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
